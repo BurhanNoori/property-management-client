70ec208f1ba6b585ffdf670160a504274354e03b
burhanNoori <burhan.batawala786@gmail.com>
2025-04-11 00:19:18 +0530

    add login signup and home links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isLogin, setLoginState] = useState(true);
+
+	const signupClickHandler = () => {
+		setLoginState(() => false);
+	};
+	return (
+
+		<Router>
+			<div className='App'>
+			<Routes>
+				<Route index element={<Navigate to='/login' replace/>}/>
+				<Route path='/login' element={<Login clickHandler={signupClickHandler}/>}/>
+				<Route path='/signup' element={<Signup/>}/>
+				<Route path='/home' element={<Home/>}/>
+			</Routes>
+				{!isLogin && <Navigate to='/signup'/>}
+			</div>
+		</Router>
+		
+			
+	);
 }
 
 export default App;
